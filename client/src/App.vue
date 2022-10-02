aae720a307603c2fd344d08a8abc340888a9ab18
mahdi karimi <mhdi_kr@outlook.com>
2022-10-02 22:20:33 +0330

    wip: toolbar stats refactor, fuzzy search, pin to top

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,127 +1,186 @@
 <template>
     <n-config-provider :theme="darkTheme">
         <div class="container">
-            <diagnostic-toolbar class="toolbar" :diagnostics="diagnostic" />
-            <clients-table :loading="isLoading" :clients="peers" />
+            <toolbar v-model:query="query" class="toolbar" :stats="stats" />
+            <clients-table :loading="loading" @dropdown-item-clicked="handleDropdownItem" :clients="filteredClients" />
         </div>
     </n-config-provider>
 </template>
 
 <style scoped>
-    .container {
-        padding: 1rem;
-        display: flex;
-        flex-direction: column;
-    }
-    .toolbar {
-        margin-bottom: 1rem;
-    }
+.container {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+}
+
+.toolbar {
+    margin-bottom: 1rem;
+}
 </style>
 
-<script lang="ts">
-import { defineComponent, shallowRef } from 'vue'
-import { darkTheme, NConfigProvider } from 'naive-ui'
-import { LogoGoogle, Cloud, Shield } from '@vicons/ionicons5'
-import ClientsTable from './components/ClientsTable.vue'
-import DiagnosticToolbar from './components/DiagnosticToolbar.vue'
-import endpoints from './endpoints'
+<script lang="ts" setup>
+import { useFuse } from "@vueuse/integrations/useFuse";
+import { computed, onBeforeUnmount, ref } from "vue";
+import { useFetch } from "@vueuse/core";
+import { darkTheme, NConfigProvider } from "naive-ui";
+import ClientsTable from "./components/ClientsTable.vue";
+import Toolbar from "./components/Toolbar.vue";
+import endpoints from "./endpoints";
 
-export default defineComponent({
-    components: {
-        NConfigProvider,
-        ClientsTable,
-        DiagnosticToolbar,
-    },
-    setup() {
-        return {
-            darkTheme,
-        }
-    },
-    data() {
-        return {
-            diagnostic: [],
-            isLoading: false,
-            isFetching: false,
-            peers: [],
-            polling: null,
-        }
-    },
-    methods: {
-        pollData: function () {
-            this.polling = setInterval(() => {
-                if (!this.isFetching) {
-                    this.isFetching = true
-                } else return
-                fetch(endpoints.CLIENTS)
-                    .then((res) => {
-                        this.isFetching = false
-                        return res.json()
-                    })
-                    .then(({ data }) => (this.peers = data.filter((i) => !!i)))
-                fetch(endpoints.DIAGNOSTICS)
-                    .then((res) => {
-                        this.isFetching = false
-                        return res.json()
-                    })
-                    .then(({ data }) => {
-                        this.diagnostic = [
-                            {
-                                message: 'wireguard service',
-                                status: data.serviceStatus,
-                                icon: shallowRef(Shield),
-                            },
-                            {
-                                message: 'cloudflare ping',
-                                status: data.pingList['1.1.1.1'],
-                                icon: shallowRef(Cloud),
-                            },
-                            {
-                                message: 'google ping',
-                                status: data.pingList['8.8.8.8'],
-                                icon: shallowRef(LogoGoogle),
-                            },
-                        ]
-                    })
-            }, 500)
+const POLLING_INTERVAL = 750; // ms
+
+const clientFetcher = useFetch(endpoints.CLIENTS).json();
+const statsFetcher = useFetch(endpoints.STATS).json();
+
+const fetchInterval = setInterval(() => {
+    if (statsFetcher.isFinished.value) {
+        statsFetcher.execute();
+    }
+    if (clientFetcher.isFinished.value) {
+        clientFetcher.execute();
+    }
+}, POLLING_INTERVAL);
+
+const loading = computed(
+    () => clients.value.length === 0 && clientFetcher.isFetching
+);
+
+const stats = computed(() => {
+    if(!statsFetcher.data.value) return { resources: {} }
+    const { data } = statsFetcher.data.value;
+    return data
+});
+
+const resources = computed(() => {
+    if (!statsFetcher.data.value) return [];
+    const {
+        data: { serverStats },
+    } = statsFetcher.data.value;
+    return [
+        {
+            name: "ram",
         },
+    ];
+});
+
+const clients = computed(() => {
+    if (!clientFetcher.data.value) return [];
+    const { data } = clientFetcher.data.value;
+    return data.filter((client) => !!client);
+    // console.log(statsFetcher.json().data.value);
+});
+
+const pins = ref([]);
+
+const handleDropdownItem = (payload) => {
+    const { option, row } = payload;
+    const key = String(option.key).toLocaleLowerCase();
+    switch (key) {
+        case "pin":
+            const indexOfFound = pins.value.indexOf(row.publicKey);
+            return indexOfFound == -1
+                ? pins.value.push(row.publicKey)
+                : pins.value.splice(indexOfFound, 1);
+        default:
+            return;
+    }
+};
+
+const query = ref("");
+
+const searchedClients = useFuse(query, clients, {
+    fuseOptions: {
+        keys: ["client", "allowedIps"],
+        isCaseSensitive: false,
+        threshold: 0.2,
     },
-    beforeDestroy() {
-        clearInterval(this.polling)
-    },
-    created() {
-        this.isLoading = true
-        if (!this.isFetching) {
-            this.isFetching = true
-        } else return
-        fetch(endpoints.CLIENTS)
-            .then((res) => {
-                this.isFetching = false
-                this.isLoading = false
-                return res.json()
-            })
-            .then(({ data }) => (this.peers = data.filter((i) => !!i)))
-        fetch(endpoints.DIAGNOSTICS)
-            .then((res) => {
-                this.isFetching = false
-                return res.json()
-            })
-            .then(({ data }) => {
-                this.diagnostic = [
-                    {
-                        message: 'wireguard service',
-                        status: data.serviceStatus,
-                    },
-                    {
-                        message: 'cloudflare ping',
-                        status: data.pingList['1.1.1.1'],
-                    },
-                    {
-                        message: 'google ping',
-                        status: data.pingList['8.8.8.8'],
-                    },
-                ]
-            })
-        this.pollData()
-    },
-})
+    matchAllWhenSearchEmpty: true,
+}).results;
+
+const filteredClients = computed(() => {
+    let clients = searchedClients.value.map(({ item }) => ({ ...item }));
+    clients = clients.map((client) => ({
+        ...client,
+        isPinned: pins.value.find((pk) => pk === client.publicKey) ? true : false,
+    }));
+    clients.sort((a, b) => b.isPinned - a.isPinned);
+    return clients;
+});
+
+onBeforeUnmount(() => clearInterval(fetchInterval));
 </script>
+
+<!-- 
+data() {
+    return {
+        stats: [],
+        resources: [],
+        isLoading: false,
+        isFetching: false,
+        peers: [],
+        polling: null,
+    };
+},
+methods: {
+    pollData: function () {
+        this.polling = setInterval(() => {
+            if (!this.isFetching) {
+                this.isFetching = true;
+            } else return console.log("return");
+            fetch(endpoints.CLIENTS)
+                .then((res) => {
+                    this.isFetching = false;
+                    return res.json();
+                })
+                .then(({ data }) => (this.peers = data.filter((i) => !!i)));
+            fetch(endpoints.STATS)
+                .then((res) => {
+                    this.isFetching = false;
+                    return res.json();
+                })
+                .then(({ data }) => {
+                    this.resources = data.serverStats;
+                    this.stats = 
+                });
+        }, 500);
+    },
+},
+beforeDestroy() {
+    clearInterval(this.polling);
+},
+created() {
+    this.isLoading = true;
+    if (!this.isFetching) {
+        this.isFetching = true;
+    } else return null;
+    fetch(endpoints.CLIENTS)
+        .then((res) => {
+            this.isFetching = false;
+            this.isLoading = false;
+            return res.json();
+        })
+        .then(({ data }) => (this.peers = data.filter((i) => !!i)));
+    fetch(endpoints.STATS)
+        .then((res) => {
+            this.isFetching = false;
+            return res.json();
+        })
+        .then(({ data }) => {
+            this.stats = [
+                {
+                    message: "wireguard service",
+                    status: data.serviceStatus,
+                },
+                {
+                    message: "cloudflare ping",
+                    status: data.pingList["1.1.1.1"],
+                },
+                {
+                    message: "google ping",
+                    status: data.pingList["8.8.8.8"],
+                },
+            ];
+        });
+    this.pollData();
+}, -->
--- client/src/components/ClientsTable.vue
+++ client/src/components/ClientsTable.vue
@@ -1,11 +1,18 @@
 <template>
-    <n-data-table size="small" :loading="loading" :columns="columns" :data="clients" :pagination="{ pageSize: 10 }" :bordered="false" />
+    <n-data-table 
+        size="small" 
+        :loading="loading" 
+        :columns="columns" 
+        :data="clients"
+        :pagination="{ pageSize: 10 }" :row-class-name="rowClassName"
+        :bordered="false"
+    />
 </template>
 
-<script lang="ts">
-import { defineComponent, h } from 'vue'
-import type { Component } from 'vue'
-import { NTable, NButton, NDataTable, NIcon, NTag, NDropdown } from 'naive-ui'
+<script lang="ts" setup>
+import { h, defineProps, defineEmits } from "vue";
+import type { Component } from "vue";
+import { NButton, NDataTable, NIcon, NTag, NDropdown } from "naive-ui";
 import {
     PlugConnected20Filled,
     PlugDisconnected28Regular,
@@ -14,128 +21,159 @@ import {
     Delete16Regular,
     Document16Regular,
     Edit16Regular,
-} from '@vicons/fluent'
+    Pin20Regular,
+} from "@vicons/fluent";
 
 const renderIcon = (icon: Component) => {
     return () => {
         return h(NIcon, null, {
             default: () => h(icon),
-        })
-    }
-}
+        });
+    };
+};
+
+const emit = defineEmits(["dropdown-item-clicked"]);
 
 const createColumns = ({ play }: { play: any }) => {
     return [
         {
-            title: 'Status',
-            key: 'status',
+            title: "Status",
+            key: "status",
             render(row: any) {
-                return h('div', {}, [
+                return h("div", {}, [
                     h(
                         NButton,
-                        { circle: true, secondary: true, type: row.status ? 'success' : 'default' },
-                        h(NIcon, { size: 20 }, [h(row.status ? PlugConnected20Filled : PlugDisconnected28Regular)])
+                        {
+                            circle: true,
+                            secondary: true,
+                            type: row.status ? "success" : "default",
+                        },
+                        h(NIcon, { size: 20 }, [
+                            h(row.status ? PlugConnected20Filled : PlugDisconnected28Regular),
+                        ])
                     ),
-                ])
+                ]);
             },
         },
         {
-            title: 'Client',
-            key: 'client',
+            title: "Client",
+            key: "client",
         },
         {
-            title: 'Download',
-            key: 'transfer.sent',
+            title: "Download",
+            key: "transfer.sent",
             render(row: any) {
-                return h('p', {}, String(row.transfer.sent))
+                return h("p", {}, String(row.transfer.sent));
             },
         },
         {
-            title: 'Upload',
-            key: 'transfer.received',
+            title: "Upload",
+            key: "transfer.received",
             render(row: any) {
-                return h('p', {}, String(row.transfer.recieved))
+                return h("p", {}, String(row.transfer.recieved));
             },
         },
         {
-            title: 'Public Key',
-            key: 'publicKey',
+            title: "Public Key",
+            key: "publicKey",
             render(row: any) {
-                return h(NTag, { size: 'small', round: true }, String(row.publicKey).slice(0, 10))
+                return h(
+                    NTag,
+                    { size: "small", round: true },
+                    String(row.publicKey).slice(0, 10)
+                );
             },
         },
         {
-            title: 'Endpoint',
-            key: 'endpoint',
+            title: "Endpoint",
+            key: "endpoint",
         },
         {
-            title: 'Allowed IPs',
-            key: 'allowedIps',
+            title: "Allowed IPs",
+            key: "allowedIps",
         },
         {
-            title: 'Last TLS Handshake',
-            key: 'lastTlsHandshake',
+            title: "Last TLS Handshake",
+            key: "lastTlsHandshake",
         },
         {
-            title: 'Action',
-            key: 'actions',
+            title: "Action",
+            key: "actions",
             render(row: any) {
-                return h(NDropdown, { options: dropDownOptions, showArrow: true }, [
-                    h(NButton, { circle: true, secondary: true }, h(NIcon, { size: 20 }, [h(MoreHorizontal32Filled)])),
-                ])
+                return h(
+                    NDropdown,
+                    {
+                        options: dropDownOptions,
+                        showArrow: true,
+                        onSelect: (_, option) => emit("dropdown-item-clicked", { option, row })
+                    },
+                    [
+                        h(
+                            NButton,
+                            {
+                                circle: true,
+                                secondary: true,
+                            },
+                            h(NIcon, { size: 20 }, [h(MoreHorizontal32Filled)])
+                        ),
+                    ]
+                );
             },
         },
-    ]
-}
+    ];
+};
+
+const rowClassName = (row) => row.isPinned ? '' : 'not-pinned'
 
 const dropDownOptions = [
     {
-        label: 'Edit Client',
-        key: 'EDIT',
+        label: "Pin to top",
+        key: "PIN",
+        icon: renderIcon(Pin20Regular),
+    },
+    {
+        label: "Edit Client",
+        key: "EDIT",
         icon: renderIcon(Edit16Regular),
     },
     {
-        label: 'Download QR Code',
-        key: 'QR',
+        label: "Download QR Code",
+        key: "QR",
         icon: renderIcon(QrCode24Filled),
     },
     {
-        label: 'Download Config File',
-        key: 'CONF',
+        label: "Download Config File",
+        key: "CONF",
         icon: renderIcon(Document16Regular),
     },
     {
-        label: 'Revoke Client',
-        key: 'REVOKE',
+        label: "Revoke Client",
+        key: "REVOKE",
         icon: renderIcon(Delete16Regular),
     },
-]
+];
+const columns = createColumns({
+    play(row: any) {
+        console.log("hello there");
+    },
+});
 
-export default defineComponent({
-    name: 'ClientsTable',
-    components: {
-        NTable,
-        NDataTable,
+const pagination = false;
+
+const props = defineProps({
+    loading: {
+        type: Boolean,
+        default: () => true,
     },
-    setup() {
-        return {
-            columns: createColumns({
-                play(row: any) {
-                    console.log('hello there')
-                },
-            }),
-            pagination: false,
-        }
+    clients: {
+        type: Array<any>,
+        required: true,
     },
-    props: {
-        loading: {
-            type: Boolean,
-            default: () => true,
-        },
-        clients: {
-            type: Array<any>,
-            required: true,
-        },
-    },
-})
+});
 </script>
+
+<style scoped>
+    :deep(.not-pinned) {
+        opacity: 0.7;
+    }
+</style>
