1258c85b340cb6d70d06be7515e7a3b28b11aa26
Alireza Kiani <alirezakiaei@gmail.com>
2022-10-07 11:58:36 +0330

    feat: implemented add and revoke client methods

--- server/src/service/wireguard.ts
+++ server/src/service/wireguard.ts
@@ -3,6 +3,7 @@ import util from 'util'
 import ping from 'ping'
 import parser from '../utils/parser'
 import { ORMInstance } from '../index'
+import { findMachinePublicIp, findUnusedIp, generateAllowedIps } from '../utils/ip'
 
 const exec = util.promisify(cp.exec)
 
@@ -42,8 +43,60 @@ export default {
      * create a new client
      * @param {any} deps:any
      */
-    createClient: async (deps: any) => {
-        return {}
+    createClient: async (deps: {
+        name: string,
+        excludeIps?: string[],
+        endpoint?: string,
+        endpointListenPort?: string
+    }) => {
+        // create private key
+        const generatePrivateKeyCommand = 'wg genkey';
+        const [wgGenkey] = await Promise.all([exec(generatePrivateKeyCommand)]);
+        const { key: clientPrivateKey} = parser(generatePrivateKeyCommand, wgGenkey.stdout);
+        // create publickey
+        const generatePublicKeyCommand = `echo ${clientPrivateKey} | wg pubkey`;
+        const [wgGenPubKey] = await Promise.all([exec(generatePublicKeyCommand)]);
+        const { key: clientPublicKey } = parser('wg pubkey', wgGenPubKey.stdout);
+        // create preshared key
+        const generatePresharedKey = 'wg genpsk';
+        const [wgGenPsk] = await Promise.all([exec(generatePresharedKey)]);
+        const { key: clientPresharedKey } = parser(generatePresharedKey, wgGenPsk.stdout);
+        // find unused ip for new client
+        const { peers, iface } = ORMInstance.selectInterface('wg0')
+        const unusedIp = findUnusedIp(
+            iface.Address,
+            peers
+        );
+        const clientAddress = `${unusedIp.IPv4.address}/${unusedIp.IPv4.range},${unusedIp.IPv6.address}/${unusedIp.IPv6.range}`;
+        const allowedIps = await generateAllowedIps(deps.excludeIps);
+        const machinePublicIp = await findMachinePublicIp();
+        // Update server conf
+        console.log(peers, iface);
+        ORMInstance.selectInterface('wg0').peers.push({
+            client: deps.name,
+            publicKey: clientPublicKey,
+            allowedIps: clientAddress,
+            presharedKey: clientPresharedKey
+        });
+        await ORMInstance.selectInterface('wg0').save()
+        await exec('wg syncconf wg0 <(wg-quick strip wg0)')
+        // create server public key
+        const generateServerPublicKeyCommand = `echo ${iface.PrivateKey} | wg pubkey`;
+        const [wgGenServerPubKey] = await Promise.all([exec(generateServerPublicKeyCommand)]);
+        const { key: serverPublicKey } = parser('wg pubkey', wgGenServerPubKey.stdout);
+        return {
+            rawContent: `[Interface]
+PrivateKey = ${clientPrivateKey}
+Address = ${clientAddress}
+DNS = 1.1.1.1,8.8.8.8
+
+[Peer]
+PublicKey = ${serverPublicKey}
+PresharedKey = ${clientPresharedKey}
+Endpoint = ${deps.endpoint || machinePublicIp}:${deps.endpointListenPort || iface.ListenPort}
+AllowedIPs = ${allowedIps}
+`
+        };
     },
     /**
      * modify an existing client fetched by its
@@ -53,5 +106,14 @@ export default {
     /**
      * revoke an existing client fetched by its public key
      */
-    revokeClient: async (pk: string) => { },
+    revokeClient: async (pk: string) => {
+        ORMInstance.selectInterface('wg0').peers = ORMInstance.selectInterface('wg0').peers.filter(p => p.publicKey !== pk);
+        console.log(ORMInstance.selectInterface('wg0').peers, pk);
+        await ORMInstance.selectInterface('wg0').save();
+        await exec('wg syncconf wg0 <(wg-quick strip wg0)');
+        return {
+            success: true,
+            deletedUserKey: pk
+        };
+    },
 }
